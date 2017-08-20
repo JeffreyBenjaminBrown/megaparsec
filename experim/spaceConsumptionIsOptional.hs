2a8e8f43a8cd0d66fff95e50fe7b48f9534d2e66
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-08-20 12:57:28 -0700

    demonstrate that space consumption is optional

---
+++ experim/spaceConsumptionIsOptional.hs
@@ -0,0 +1,22 @@
+module Dwt.Parse where
+
+import Control.Applicative (empty)
+import Data.Void (Void)
+import Text.Megaparsec
+import qualified Text.Megaparsec.Char as C
+import qualified Text.Megaparsec.Char.Lexer as L
+
+type Parser = Parsec Void String
+
+demo :: IO ()
+demo = parseTest f "st"
+  where f = do sc
+               x <- C.string "s" <* sc
+               y <- C.string "t" <* sc
+               return $ x ++ y
+
+sc :: Parser ()
+sc = L.space C.space1 empty empty
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc
